f918c4d781a6427c657b9c037b57c7676d0ba41d
Mihovna <mixovna@gmail.com>
2018-10-17 20:50:20 +0300

    delete

--- lesson3/public/index.js
+++
@@ -1,8 +0,0 @@
-var imyaFunkcii = function (vvodimoeZnachenie) {
-    var vremenaGoda = ['зима','весна','лето','осень'];
-    if (vremenaGoda.indexOf(vvodimoeZnachenie)=== -1) {
-        return false;
-    }
-    return true;
-};
-alert(imyaFunkcii('зиа'))
